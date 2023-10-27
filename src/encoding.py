a330d9f56362401143b8b524ed58669820f7cf36
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-10-27 10:36:30 +0530

    Implemented enum for max number of bits

--- src/encoding.py
+++ src/encoding.py
@@ -13,6 +13,12 @@ class ErrorCorrectionLevel(Enum):
     Q = {'Numeric': 111, 'Alpha Numeric': 67, 'Byte': 46, 'Kanji': 28}
     H = {'Numeric': 82, 'Alpha Numeric': 50, 'Byte': 34, 'Kanji': 21}
 
+class NumberOfBits(Enum):
+    NUMERIC = 10
+    ALPHANUMERIC = 9
+    BYTE = 8
+    KANJI = 8
+
 def get_mode_indicator(mode: Mode) -> str:
     return {
         Mode.NUMERIC: '0001',

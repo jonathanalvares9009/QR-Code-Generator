500992aab0de3fbb2e726e482bc3e0c5f8b2fee3
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-10-27 12:44:28 +0530

    Created encoding for alphanumeric values

--- src/encoding.py
+++ src/encoding.py
@@ -22,3 +22,29 @@ def get_encoded_numeric_payload(data: str) -> str:
         else:
             payload += bin(int(group))[2:].zfill(4)
     return payload
+
+def get_alphanumeric_number_representation(character: str) -> int:
+    mapping = {
+        '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+        'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15, 'G': 16, 'H': 17, 'I': 18, 'J': 19,
+        'K': 20, 'L': 21, 'M': 22, 'N': 23, 'O': 24, 'P': 25, 'Q': 26, 'R': 27, 'S': 28, 'T': 29,
+        'U': 30, 'V': 31, 'W': 32, 'X': 33, 'Y': 34, 'Z': 35, ' ': 36, '$': 37, '%': 38, '*': 39,
+        '+': 40, '-': 41, '.': 42, '/': 43, ':': 44
+    }
+    return mapping.get(character, None)
+
+def get_encoded_alphanumeric_payload(data: str) -> str:
+    """Returns the encoded payload for alphanumeric mode"""
+    payload = ""
+    # Break the data into groups of 2
+    for i in range(0, len(data), 2):
+        # Get the current group
+        group = data[i:i+2]
+        if len(group) == 2:
+            payload += bin(
+                get_alphanumeric_number_representation(group[0]) * 45 +
+                get_alphanumeric_number_representation(group[1])
+            )[2:].zfill(11)
+        else:
+            payload += bin(get_alphanumeric_number_representation(group[0]))[2:].zfill(11)
+    return payload
--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,5 +1,5 @@
 from src.models import Mode
-from src.encoding import get_encoded_numeric_payload, get_mode_indicator
+from src.encoding import get_encoded_alphanumeric_payload, get_encoded_numeric_payload, get_mode_indicator
 
 def test_is_encoding_mode_indicator_numerical():
     assert get_mode_indicator(Mode.NUMERIC) == '0001'
@@ -17,3 +17,6 @@ def test_is_valid_encoded_numeric_payload():
     assert get_encoded_numeric_payload("291") == '0100100011'
     assert get_encoded_numeric_payload("76") == '1001100'
     assert get_encoded_numeric_payload("4") == '0100'
+
+def test_is_valid_encoded_alphanumeric_payload():
+    assert get_encoded_alphanumeric_payload("HELLO CC WORLD") == '01100001011011110001101000101110001000101000110011101001000101001101110111110'

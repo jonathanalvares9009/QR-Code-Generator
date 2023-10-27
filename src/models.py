cf68f0b8ffbbd958b20ce7e033e073c894adf7d3
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-10-27 11:59:35 +0530

    Refactored encoding module

--- src/models.py
+++ src/models.py
@@ -7,3 +7,20 @@ class Mode(Enum):
     BYTE = "BYTE"
     KANJI = "KANJI"
     
+class Level(Enum):
+    L = 'L'
+    M = 'M'
+    Q = 'Q'
+    H = 'H'
+
+class ErrorCorrectionLevel(Enum):
+    L = {'Numeric': 187, 'Alpha Numeric': 114, 'Byte': 78, 'Kanji': 48}
+    M = {'Numeric': 149, 'Alpha Numeric': 90, 'Byte': 62, 'Kanji': 38}
+    Q = {'Numeric': 111, 'Alpha Numeric': 67, 'Byte': 46, 'Kanji': 28}
+    H = {'Numeric': 82, 'Alpha Numeric': 50, 'Byte': 34, 'Kanji': 21}
+
+class NumberOfBits(Enum):
+    NUMERIC = 10
+    ALPHANUMERIC = 9
+    BYTE = 8
+    KANJI = 8

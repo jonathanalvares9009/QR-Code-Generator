35d12510b38bed558690d845ab088341a7a0d05a
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-10-27 12:44:09 +0530

    Check to see if lowercase alphabets are not alphanumeric

--- tests/test_mode.py
+++ tests/test_mode.py
@@ -11,6 +11,11 @@ def test_is_mode_alphanumeric():
     mode = get_encoding_mode(data)
     assert mode == Mode.ALPHANUMERIC
 
+def test_is_not_mode_alphanumeric():
+    data = "abc90"
+    mode = get_encoding_mode(data)
+    assert mode != Mode.ALPHANUMERIC
+
 def test_is_mode_byte():
     data = "ÕÉē90ABC"
     mode = get_encoding_mode(data)

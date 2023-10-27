2e25baf68e8447263163752109155e454b6f1712
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-10-27 09:48:38 +0530

    Finished implementation of get_encoding_mode

--- src/encoding_mode.py
+++ src/encoding_mode.py
@@ -1,5 +1,12 @@
+import re
 from src.models import Mode
 
 def get_encoding_mode(data: str) -> Mode:
     """Get the encoding mode for the given data"""
-    return Mode.NUMERIC
+    if re.match(r'^[\u4e00-\u9faf]+$', data):
+        return Mode.KANJI
+    if re.match(r'^[0-9]+$', data):
+        return Mode.NUMERIC
+    if re.match(r'^[0-9A-Z $%*+./:-]+$', data):
+        return Mode.ALPHANUMERIC
+    return Mode.BYTE

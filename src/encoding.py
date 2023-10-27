35f03baac4e0b2f20e250b0ee53c79922c4a0e38
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-10-27 12:10:02 +0530

    Numeric encoding implemented

--- src/encoding.py
+++ src/encoding.py
@@ -8,4 +8,17 @@ def get_mode_indicator(mode: Mode) -> str:
         Mode.KANJI: '1000',
     }[mode]
 
-# def get_encoded_data(data: str, level: Level = Level.L) -> str:
+def get_encoded_numeric_payload(data: str) -> str:
+    """Returns the encoded payload for numeric mode"""
+    payload = ""
+    # Break the data into groups of 3
+    for i in range(0, len(data), 3):
+        # Get the current group
+        group = data[i:i+3]
+        if int(group) > 99:
+            payload += bin(int(group))[2:].zfill(10)
+        elif int(group) > 9:
+            payload += bin(int(group))[2:].zfill(7)
+        else:
+            payload += bin(int(group))[2:].zfill(4)
+    return payload
--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,5 +1,5 @@
 from src.models import Mode
-from src.encoding import get_mode_indicator
+from src.encoding import get_encoded_numeric_payload, get_mode_indicator
 
 def test_is_encoding_mode_indicator_numerical():
     assert get_mode_indicator(Mode.NUMERIC) == '0001'
@@ -12,3 +12,8 @@ def test_is_encoding_mode_indicator_byte():
 
 def test_is_encoding_mode_indicator_kanji():
     assert get_mode_indicator(Mode.KANJI) == '1000'
+
+def test_is_valid_encoded_numeric_payload():
+    assert get_encoded_numeric_payload("291") == '0100100011'
+    assert get_encoded_numeric_payload("76") == '1001100'
+    assert get_encoded_numeric_payload("4") == '0100'

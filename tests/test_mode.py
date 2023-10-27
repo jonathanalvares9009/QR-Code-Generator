df5aa4da9d1e0aa933028eebe19b936fec282c8f
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-10-27 09:34:13 +0530

    Wrote a test to check if mode is numeric

---
+++ tests/test_mode.py
@@ -0,0 +1,7 @@
+from src.models import Mode
+from src.encoding_mode import get_encoding_mode
+
+def test_is_mode_numeric():
+    data = "123"
+    mode = get_encoding_mode(data)
+    assert mode == Mode.NUMERIC

8be33353705a784eb4b12431ffcd888bf6f5ecb1
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-10-27 10:33:19 +0530

    Created max allowed characters for version 4

--- src/encoding.py
+++ src/encoding.py
@@ -7,6 +7,12 @@ class Level(Enum):
     Q = 'Q'
     H = 'H'
 
+class ErrorCorrectionLevel(Enum):
+    L = {'Numeric': 187, 'Alpha Numeric': 114, 'Byte': 78, 'Kanji': 48}
+    M = {'Numeric': 149, 'Alpha Numeric': 90, 'Byte': 62, 'Kanji': 38}
+    Q = {'Numeric': 111, 'Alpha Numeric': 67, 'Byte': 46, 'Kanji': 28}
+    H = {'Numeric': 82, 'Alpha Numeric': 50, 'Byte': 34, 'Kanji': 21}
+
 def get_mode_indicator(mode: Mode) -> str:
     return {
         Mode.NUMERIC: '0001',
